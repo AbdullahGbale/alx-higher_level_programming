8f26f43a91fce29c416dcafeb50eca2f9157ea5b
AbdullahGbale <bigimarniac@gmail.com>
2023-10-02 03:11:54 -0700

    try

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+"""Module: Locked class"""
 class LockedClass:
+    """
+    Class: LockedClass
+    """
     def __setattr__(self, name, value):
+         """
+        Set instance attribute 'name' to 'value' if 'name' is 'first_name'.
+        Raise an AttributeError for any other attribute.
+
+        Args:
+            name (str): The name of the attribute being set.
+            value: The value to set the attribute to.
+
+        Raises:
+            AttributeError: If 'name' is not 'first_name'.
+        """
         if name == "first_name":
             self.__dict__[name] = value
 
