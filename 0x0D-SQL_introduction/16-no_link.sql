9b9fa8fd2baf93a95bc71882e64675c172d30409
AbdullahGbale <bigimarniac@gmail.com>
2023-12-14 14:58:20 -0800

    continue

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,3 @@
 -- script that lists all records of the table second_table of hbtn_0c_0
-SELECT score, name
-FROM hbtn_0c_0.second_table
-WHERE name IS NOT NULL
+SELECT score, name FROM hbtn_0c_0.second_table WHERE name IS NOT NULL
 ORDER BY score DESC;
