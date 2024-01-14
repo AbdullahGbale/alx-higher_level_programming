f7b1c8a1fc333f3bb3208ea277b6fed798e3a8ff
AbdullahGbale <bigimarniac@gmail.com>
2024-01-14 14:40:52 -0800

    try

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Script that lists all states from a database where name matches the
-argument, safe from MySQL injections
+Script that lists all states with a name starting with N from a database
 """
 
 if __name__ == "__main__":
@@ -12,8 +11,10 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cur.execute(query, (sys.argv[4],))
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+            cities INNER JOIN states ON states.id=cities.state_id""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
+    cur.close()
+    db.close()
